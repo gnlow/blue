039ff5cfd4dfa45d0511439045e63be58b6a5b77
Gnlow <entrydark@gmail.com>
2021-12-01 11:14:35 +0900

    change name

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,23 @@ export const rgb = (r: number, g: number, b: number) => ({r, g, b})
 
 type Op<X, Y> = (x: X) => Y
 
-export function $<A, B>(op1: Op<A, B>,): (a: A) => B
-export function $<A, B, C>(
+export function pipe<A, B>(op1: Op<A, B>,): (a: A) => B
+export function pipe<A, B, C>(
     op1: Op<A, B>,
     op2: Op<B, C>,
 ): (a: A) => C
-export function $<A, B, C, D>(
+export function pipe<A, B, C, D>(
     op1: Op<A, B>,
     op2: Op<B, C>,
     op3: Op<C, D>,
 ): (a: A) => C
-export function $(...fs: ((a: unknown) => unknown)[]) {
+export function pipe<A, B, C, D, E>(
+    op1: Op<A, B>,
+    op2: Op<B, C>,
+    op3: Op<C, D>,
+    op4: Op<D, E>,
+): (a: A) => E
+export function pipe(...fs: ((a: unknown) => unknown)[]) {
     return (init: unknown) => fs.reduce((v, f) => f(v), init)
 }
 
@@ -28,8 +34,8 @@ const base =
     (text: string | TemplateStringsArray) =>
         f(String(text), color)
 
-export const b = $(strz, bold)
-export const i = $(strz, italic)
+export const b = pipe(strz, bold)
+export const i = pipe(strz, italic)
 
 export const c = base(rgb24)
 export const bg = base(bgRgb24)
--- test.ts
+++ test.ts
@@ -1,3 +1,11 @@
-import { $, b, i, c, bg, pink } from "./mod.ts"
+import {
+    pipe as $, b, i, c, bg,
+    pink, black, white
+} from "./mod.ts"
 
-console.log($(b, i, c(pink))`Hello, World!`)
+console.log(
+    $(c(pink), bg(black))
+    `Hello,`,
+    $(c(black), bg(white), b, i)
+    `World!`
+)
